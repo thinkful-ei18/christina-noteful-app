93c74a0e9876770f81b9c4bc1d5f025caa8e4625
Christina Moore <sayhitochristina@gmail.com>
2018-01-29 15:10:01 -0600

    day 1 complete

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
 'use strict';
 
 const data = require('./db/notes');
+const express = require('express');
 
-console.log('hello world!');
 
 // INSERT EXPRESS APP CODE HERE...
+
+const app = express();
+app.use(express.static('public'));
+
+
+
+// app.get('/v1/notes/hi', (req, res) => {
+//   res.send('ok');
+// });
+
+app.get('/v1/notes/:id', (req, res) => {
+  const { id } = req.params;
+  const passIdInt = parseInt(id);
+  const note = data.find(item => item.id === passIdInt);
+  res.json(note);
+});
+
+// search
+
+app.get('/v1/notes', (req, res) => {
+  const { searchTerm } = req.query;
+  // search through /notes
+  let filterNotes = searchTerm ? data.filter(word => word.title.includes(searchTerm)) : data; 
+  res.json(filterNotes);
+  console.log(req.query);
+});
+
+
+
+
+
+app.listen(8080, function () {
+  console.info(`server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
