eb94fe79f6b0414f5b579c468015264f9f9afb09
Christina Moore <sayhitochristina@gmail.com>
2018-01-30 13:52:20 -0600

    for debug

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const data = require('./db/notes');
 const express = require('express');
 const { PORT } = require('./config');
 const logger = require('./middleware');
+const simDB = require('./db/simDB');
+const notes = simDB.initialize(data);
 
 
 // INSERT EXPRESS APP CODE HERE...
@@ -20,18 +22,56 @@ app.get('/v1/notes/:id', (req, res) => {
   res.json(note);
 });
 
+
+
 // search
 
-app.get('/v1/notes', (req, res) => {
+// app.get('/v1/notes', (req, res, next) => {
+//   const { searchTerm } = req.query;
+//   console.log(searchTerm);
+//   // search through /notes
+//   //let filterNotes = searchTerm ? data.filter(word => word.title.includes(searchTerm)) : data; 
+//   //res.json(filterNotes);
+//   //console.log(req.query);
+//   notes.filter(searchTerm, (err, list) => {
+//     if (err) {
+//       return next(err);
+//     } else {
+//       res.json(list);
+//       console.log('here');
+//     }
+//   });
+// });
+
+app.get('/v1/notes', (req, res, next) => {
+  console.log('test');
   const { searchTerm } = req.query;
-  // search through /notes
-  let filterNotes = searchTerm ? data.filter(word => word.title.includes(searchTerm)) : data; 
-  res.json(filterNotes);
-  console.log(req.query);
+
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err);
+    }
+    res.json(list);
+  });
 });
 
 
 
+
+
+app.use(function (req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
 
 
 app.listen(PORT, function () {
